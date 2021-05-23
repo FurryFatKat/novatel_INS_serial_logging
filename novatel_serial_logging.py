1267cfb5e74ffb8f9e6f5c1eb8925f41b1173fc9
FurryFatKat <joe.hz.chen@gmail.com>
2021-05-23 14:53:26 -0600

    Added KeyboardInterrupt to terminate program

--- novatel_serial_logging.py
+++ novatel_serial_logging.py
@@ -30,13 +30,18 @@ DEFAULT_LOGLIST =[
     'LOG INSCONFIGB ONCHANGED',
     'LOG INSUPDATESTATUSB ONNEW']
 
+INS_CONFIG = [
+    'connectimu {} {}',
+    'setinsrotation RBV {} {} {} 3 3 3',
+    'setinstranslation ANT1 {} {} {} 0.05 0.05 0.05']
+
 def establish_serial_connection(port,baudrate):
     serialport = serial.Serial(port=port)
     serialport.timeout = 5
     serialport.send_break()
-    time.sleep(0.2)
+    time.sleep(2)
     serialport.send_break()
-    time.sleep(0.2)
+    time.sleep(2)
     # loop through supported baud rate
     for baud in SUPPORTED_BAUD:
         logging.info('connecting at {}...'.format(baud))
@@ -45,32 +50,38 @@ def establish_serial_connection(port,baudrate):
         time.sleep(0.2)
         serialport.flushInput()
         serialport.write('serialconfig {}\r'.format(baudrate).encode())
+        time.sleep(0.2)
         # check for response
         if b'<OK' in serialport.read(10):
             logging.info('baud rate changed to {}'.format(baudrate))
             serialport.timeout = None
+            PORT_FOUND = True
             break
-
+    
+    if PORT_FOUND == False:
+        raise ValueError('Serial communication not established.')
     return serialport
 
 def read_from_serial(port, filename):
     outputFile = open(filename,'wb')
     incomingData = bytearray()
+    logging.info('start reading from port...')
     try:
-        while True:
+        while READ:
             if port.inWaiting():
                 incomingData = port.read(port.inWaiting())
                 if len(incomingData) > 0:
+                    logging.debug(incomingData)
                     outputFile.write(incomingData)
                     outputFile.flush()
                     incomingData = bytearray()
             else:
                 time.sleep(0.1)
     except KeyboardInterrupt:
-        if len(incomingData) > 0:
-            outputFile.write(incomingData)
-            outputFile.flush()
-        outputFile.close()
+        # TODO::this can be changed to detect serial port exception in the future
+        logging.info('KeyboardInterrupt captured in read thread')
+    outputFile.close()
+    logging.info('END OF READ THREAD.')
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='NovAtel Receiver Data Logging')
@@ -117,6 +128,12 @@ def parse_arguments():
     return parser.parse_args()
 
 def main():
+    global READ
+    global PORT_FOUND
+    logging.getLogger().setLevel(logging.INFO)
+
+    READ = True
+    PORT_FOUND = False
     args = parse_arguments()
     comport = establish_serial_connection(port=args.c.upper(), baudrate=args.b)
 
@@ -129,18 +146,26 @@ def main():
         if not args.ip or not args.la or not args.rbv:
             raise ValueError('Missing Input for Configuring INS: ip/LA/RBV')
         else:
-            comport.write('connectimu {} {}\r'.formate(args.ip,args.i).encode())
-            comport.write('setinsrotation RBV {} {} {} 3 3 3\r'.formate(args.la[0],args.la[1],args.la[2]).encode())
-            comport.write('setinstranslation ANT1 {} {} {} 0.05 0.05 0.05\r'.formate(args.rbv[0],args.rbv[1],args.rbv[2]).encode())
-
+            # TODO::better code can be written but this works for now
+            comport.write((INS_CONFIG[0] + '\r').format(args.ip,args.i).encode())
+            logging.info('sending '+ INS_CONFIG[0].format(args.ip,args.i))
+            comport.write((INS_CONFIG[1] + '\r').format(args.rbv[0],args.rbv[1],args.rbv[2]).encode())
+            logging.info('sending '+ INS_CONFIG[1].format(args.rbv[0],args.rbv[1],args.rbv[2]))
+            comport.write((INS_CONFIG[2] + '\r').format(args.la[0],args.la[1],args.la[2]).encode())
+            logging.info('sending '+ INS_CONFIG[2].format(args.la[0],args.la[1],args.la[2]))
+            
     # send default SPAN logging profile 
     for log in DEFAULT_LOGLIST:
         logging.info('sending {}...'.format(log))
         comport.write((log + '\r').encode())
         time.sleep(0.1)
 
-
-    readthread.join()
+    try:
+        while True:
+            time.sleep(10)
+    except KeyboardInterrupt:
+        READ = False
+        logging.info('Program terminated.')
 
 
 if __name__ == "__main__":
